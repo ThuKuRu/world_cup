8f41401b1d23525dbf7aeef989923dec084a0e24
ThuKuRu <101419174+ThuKuRu@users.noreply.github.com>
2023-07-20 15:40:16 +0700

    Create rank frame

---
+++ wc/src/containers/Ranks/index.style.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+
+const RankStyle = styled.div`
+  display: flex;
+  margin-top: 60px;
+  justify-content: center;
+  margin-bottom: 30px;
+  .center {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    @media (max-width: 1280px) {
+      grid-template-columns: repeat(1, 1fr);
+    }
+    .component {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      .title {
+        display: flex;
+        align-items: center;
+        margin: 0.5em 0.5em 0.5em 1em;
+        justify-content: flex-start;
+        font-weight: 700;
+        font-size: 20px;
+        p {
+          margin: 10px;
+        }
+      }
+      .table {
+        width: 600px;
+        font-weight: 600;
+        background-color: #d9d9d9;
+        align-items: center;
+        border-radius: 1em;
+        margin-left: 2em;
+        .header {
+          display: grid;
+          grid-template-columns: repeat(9, 1fr);
+          border-bottom: 2px solid #fff;
+          .td {
+            display: flex;
+            justify-content: center;
+            padding: 0.5em;
+            align-items: center;
+          }
+          .teamName {
+            width: 100px;
+          }
+          .nation {
+            width: 50px;
+          }
+        }
+        .tr {
+          display: grid;
+          grid-template-columns: repeat(9, 1fr);
+          border-bottom: 1px solid #fff;
+          .td {
+            padding: 0.5em;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+
+          .nation {
+            width: 50px;
+            .img {
+              margin-top: 0.5em;
+            }
+          }
+          .teamName {
+            width: 100px;
+          }
+        }
+      }
+    }
+  }
+`;
+
+export { RankStyle };
